1749e2fb85842974b4b95c7fbc1be00cb5885643
twagner <twagner@student.42.fr>
2021-10-30 14:59:58 +0200

    protected strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 10:00:20 by twagner           #+#    #+#             */
-/*   Updated: 2021/10/26 10:11:46 by twagner          ###   ########.fr       */
+/*   Updated: 2021/10/30 13:47:07 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strdup(const char *s1)
 {
 	char	*dup;
 
+	if (!s1)
+		return (NULL);
 	dup = (char *)malloc(sizeof(*dup) * (ft_strlen(s1) + 1));
 	if (!dup)
 		return (NULL);
