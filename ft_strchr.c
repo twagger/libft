73c4c9abbe21bcf797520c73fa38557c5c1c0653
twagger <twagner@student.42.fr>
2022-01-11 16:23:32 +0100

    protection strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: twagner <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 07:54:19 by twagner           #+#    #+#             */
-/*   Updated: 2021/05/21 08:00:26 by twagner          ###   ########.fr       */
+/*   Updated: 2022/01/11 12:27:22 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (!s)
+		return (NULL);
 	while (*s)
 	{
 		if (*s == (char)c)
